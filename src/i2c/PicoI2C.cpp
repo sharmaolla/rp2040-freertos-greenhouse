3ff049458a8f0427823876df28dff7dc5962cb9c
Keijo Länsikunnas <keijo.lansikunnas@metropolia.fi>
2024-10-02 15:43:16 +0300

    Reduced interrupt load and added some optional debugs.

--- src/i2c/PicoI2C.cpp
+++ src/i2c/PicoI2C.cpp
@@ -5,6 +5,12 @@
 #include "pico/stdlib.h"
 #include "PicoI2C.h"
 
+//#define DEBUG_PRINT_TASK
+
+#ifdef DEBUG_PRINT_TASK
+#include "DebugTask.h"
+#endif
+
 #define I2C0_SDA_PIN 16
 #define I2C0_SCL_PIN 17
 
@@ -47,17 +53,23 @@ PicoI2C::PicoI2C(uint bus_nr, uint speed) :
     gpio_pull_up(scl);
     gpio_init(sda);
     gpio_pull_up(sda);
+    irq_set_enabled(irqn, false);
+    irq_set_exclusive_handler(irqn, bus_nr ? i2c1_irq : i2c0_irq);
     i2c_init(i2c, speed);
     gpio_set_function(sda, GPIO_FUNC_I2C);
     gpio_set_function(scl, GPIO_FUNC_I2C);
-    irq_set_enabled(irqn, false);
-    irq_set_exclusive_handler(irqn, bus_nr ? i2c1_irq : i2c0_irq);
+    // Set FIFO watermarks
+    i2c->hw->tx_tl = 0; // TX_FIFO watermark to 0
+    i2c->hw->rx_tl = 14; // RX_FIFO watermark to 15 (manual gives impression that level is one higher than reg value)
     if (bus_nr) i2c1_instance = this;
     else i2c0_instance = this;
 }
 
 
 void PicoI2C::tx_fill_fifo() {
+#ifdef DEBUG_PRINT_TASK
+    int fill{0};
+#endif
     while (wctr > 0 && i2c_get_write_available(i2c) > 0) {
         bool last = wctr == 1;
         bool stop = rctr == 0;
@@ -72,11 +84,21 @@ void PicoI2C::tx_fill_fifo() {
         --wctr;
 
         if (last && !stop) i2c->restart_on_next = true;
+#ifdef DEBUG_PRINT_TASK
+        ++fill;
+#endif
     }
+#ifdef DEBUG_PRINT_TASK
+    DebugTask::debug("tx_fill: %d", fill);
+#endif
 }
 
 
 void PicoI2C::rx_fill_fifo() {
+#ifdef DEBUG_PRINT_TASK
+    int fill{0};
+#endif
+
     while (rctr > 0 && i2c_get_write_available(i2c) > 0) {
         bool last = rctr == 1;
         i2c->hw->data_cmd =
@@ -88,7 +110,14 @@ void PicoI2C::rx_fill_fifo() {
         // clear restart bit after first command
         if (i2c->restart_on_next) i2c->restart_on_next = false;
         --rctr;
+#ifdef DEBUG_PRINT_TASK
+        ++fill;
+#endif
     }
+
+#ifdef DEBUG_PRINT_TASK
+    DebugTask::debug("rx_fill: %d", fill);
+#endif
 }
 
 
@@ -110,7 +139,7 @@ uint PicoI2C::transaction(uint8_t addr, const uint8_t *wbuffer, uint wlength, ui
     i2c->hw->enable = 0;
     i2c->hw->tar = addr;
     i2c->hw->enable = 1;
-    i2c->hw->intr_mask = I2C_IC_INTR_MASK_M_STOP_DET_BITS | I2C_IC_INTR_MASK_M_TX_EMPTY_BITS;
+    i2c->hw->intr_mask = I2C_IC_INTR_MASK_M_STOP_DET_BITS | I2C_IC_INTR_MASK_M_TX_EMPTY_BITS | I2C_IC_INTR_MASK_M_RX_FULL_BITS;
     i2c->restart_on_next = false;
     // setup transfer
     wbuf = wbuffer;
@@ -142,15 +171,32 @@ uint PicoI2C::transaction(uint8_t addr, const uint8_t *wbuffer, uint wlength, ui
 
 void PicoI2C::isr() {
     BaseType_t hpw = pdFALSE;
+#ifdef DEBUG_PRINT_TASK
+    DebugTask::debug("%d %d %d %d",
+        !!(i2c->hw->raw_intr_stat & I2C_IC_RAW_INTR_STAT_STOP_DET_BITS),
+        !!(i2c->hw->raw_intr_stat & I2C_IC_RAW_INTR_STAT_RX_FULL_BITS),
+        !!(i2c->hw->raw_intr_stat & I2C_IC_RAW_INTR_STAT_TX_EMPTY_BITS),
+        !!(i2c->hw->raw_intr_stat & I2C_IC_RAW_INTR_STAT_RX_OVER_BITS)
+        );
+#endif
     // See if we have active read and data available.
     // Read is paced writes to command register in master mode
     // so we don't need RX_FULL interrupt.
     // We just empty the rxfifo before additional writes to cmd register
     //  Testing (i2c->hw->status & I2C_IC_STATUS_RFNE_BITS) works also
+#ifdef DEBUG_PRINT_TASK
+    int fill{0};
+#endif
     while (rcnt > 0 && i2c->hw->rxflr > 0) {
         *rbuf++ = static_cast<uint8_t>(i2c->hw->data_cmd);
         --rcnt;
+#ifdef DEBUG_PRINT_TASK
+        ++fill;
+#endif
     }
+#ifdef DEBUG_PRINT_TASK
+    DebugTask::debug("read: %d, %d", fill, rcnt);
+#endif
 
     if (i2c->hw->intr_stat & I2C_IC_INTR_MASK_M_TX_EMPTY_BITS) {
         // write commands go first
@@ -159,11 +205,10 @@ void PicoI2C::isr() {
         } else if (rctr > 0) {
             rx_fill_fifo();
         }
-        if (wctr == 0 && rcnt == 0) { // note! using rcnt (=received byte counter) for disabling TX_EMPTY
-            // we must let TX_EMPTY to trigger once after last read command to get all bytes.
-            // we are done with sending write/read commands
-            // mask all other interrupts except stop
-            i2c->hw->intr_mask = I2C_IC_INTR_MASK_M_STOP_DET_BITS;
+        if (wctr == 0 && rctr == 0) {
+            // Disable TX_EMPTY interrupt when we are done with write and read commands
+            // keep receive and stop interrupts active to catch received bytes and stop
+            i2c->hw->intr_mask = I2C_IC_INTR_MASK_M_STOP_DET_BITS | I2C_IC_INTR_MASK_M_RX_FULL_BITS;
         }
     }
 
